e7e72c9ed0253ac099080fd006b5f282e314a024
manticorevault <artur_seilah@hotmail.com>
2020-11-26 18:50:16 +0000

    Added the fetch function to the User model as well as a json server from where it's fetching data from

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { User } from './models/User'
 
-const user = new User({ name: 'Artur', age: 20 })
+const user = new User({ id: 1 })
 
-user.on('change', () => {})
-user.on('change', () => {})
-user.on('click', () => {})
+user.fetch()
 
-console.log(user)
+setTimeout(() => {
+  console.log(user)
+}, 4000)
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,7 @@
+import axios, { AxiosResponse } from 'axios'
+
 interface UserProps {
+  id?: number
   name?: string
   age?: number
 }
@@ -22,5 +25,25 @@ export class User {
     const handlers = this.events[eventName] || []
     handlers.push(callback)
     this.events[eventName] = handlers
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName]
+
+    if (!handlers || handlers.length === 0) {
+      return
+    }
+
+    handlers.forEach((callback) => {
+      callback()
+    })
+  }
+
+  fetch(): void {
+    axios
+      .get(`http://localhost:3000/users/${this.get('id')}`)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data)
+      })
   }
 }
