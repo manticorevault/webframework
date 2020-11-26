ab8309383978841f7e8d21940f782af52722cec2
manticorevault <artur_seilah@hotmail.com>
2020-11-26 18:01:24 +0000

    Started adding the events handler implementation

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -117,7 +117,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"src/models/user.ts":[function(require,module,exports) {
+})({"src/models/User.ts":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -130,6 +130,7 @@ var User =
 function () {
   function User(data) {
     this.data = data;
+    this.events = {};
   }
 
   User.prototype.get = function (propName) {
@@ -138,6 +139,12 @@ function () {
 
   User.prototype.set = function (update) {
     Object.assign(this.data, update);
+  };
+
+  User.prototype.on = function (eventName, callback) {
+    var handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
   };
 
   return User;
@@ -151,18 +158,17 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var user_1 = require("./models/user");
+var User_1 = require("./models/User");
 
-var user = new user_1.User({
+var user = new User_1.User({
   name: 'Artur',
   age: 20
 });
-user.set({
-  name: 'Artur Alves Serra'
-});
-console.log(user.get('name'));
-console.log(user.get('age'));
-},{"./models/user":"src/models/user.ts"}],"../../../.nvm/versions/node/v12.18.3/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+user.on('change', function () {});
+user.on('change', function () {});
+user.on('click', function () {});
+console.log(user);
+},{"./models/User":"src/models/User.ts"}],"../../../.nvm/versions/node/v12.18.3/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
