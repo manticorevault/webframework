05e4228c6979ccc7b1e0721d45a39184a8624b8a
manticorevault <artur_seilah@hotmail.com>
2020-11-27 14:18:23 +0000

    User data can now be updated on the database

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -1959,6 +1959,16 @@ function () {
     });
   };
 
+  User.prototype.save = function () {
+    var id = this.get('id');
+
+    if (id) {
+      axios_1.default.put("http://localhost:3000/users/" + id, this.data);
+    } else {
+      axios_1.default.post('http://localhost:3000/users', this.data);
+    }
+  };
+
   return User;
 }();
 
@@ -1973,12 +1983,10 @@ Object.defineProperty(exports, "__esModule", {
 var User_1 = require("./models/User");
 
 var user = new User_1.User({
-  id: 1
+  name: 'new record',
+  age: 2
 });
-user.fetch();
-setTimeout(function () {
-  console.log(user);
-}, 4000);
+user.save();
 },{"./models/User":"src/models/User.ts"}],"../../../.nvm/versions/node/v12.18.3/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -2007,7 +2015,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34527" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45027" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
