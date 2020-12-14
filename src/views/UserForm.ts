3193be51c47215729c6ed5264e0a2bb5a60283c9
manticorevault <artur_seilah@hotmail.com>
2020-12-14 21:00:59 +0000

    Re-renders the model HTML on an event

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,19 +1,24 @@
+import { User } from '../models/User'
+
 export class UserForm {
-  constructor(public parent: Element) {}
+  constructor(public parent: Element, public model: User) {
+    this.bindModel()
+  }
+
+  bindModel(): void {
+    this.model.on('change', () => {
+      this.render()
+    })
+  }
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      'click:button': this.onButtonClick,
-      'mouseenter:h1': this.onHeaderHover,
+      'click:.set-age': this.onSetAgeClick,
     }
   }
 
-  onButtonClick(): void {
-    console.log('Hello!')
-  }
-
-  onHeaderHover(): void {
-    console.log('Hovered over H1')
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge()
   }
 
   template(): string {
@@ -23,9 +28,18 @@ export class UserForm {
                     User Form
                 </h1>
 
+                <div>
+                    User name: ${this.model.get('name')}
+                </div>
+
+                <div>
+                    User age: ${this.model.get('age')}
+                </div>
+
                 <input />
 
                 <button> Click Me! </button>
+                <button class="set-age"> Set Random Age </button>
             </div>
         `
   }
@@ -43,11 +57,17 @@ export class UserForm {
   }
 
   render(): void {
+    // Remove the previous HTML from the parent element
+    this.parent.innerHTML = ''
+
+    // Re-render the template
     const templateElement = document.createElement('template')
     templateElement.innerHTML = this.template()
 
+    // Bind the events
     this.bindEvents(templateElement.content)
 
+    // Stick the events to the parent element
     this.parent.append(templateElement.content)
   }
 }
